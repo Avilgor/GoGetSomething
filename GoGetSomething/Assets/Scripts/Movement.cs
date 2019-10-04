4cb29c2ed35456d83ec8f030bad24413397e5793
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-10-05 01:49:25 +0200

    Update

---
+++ GoGetSomething/Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    enum playerState
+    {
+        idle = 0,
+        death,
+        walkRight,
+        walkLeft,
+        walkUp,
+        walkDown,
+        punch,
+
+    }
+
+    playerState currentState;
+
+    void Start()
+    {
+        currentState = playerState.idle;
+    }
+
+    
+    void Update()
+    {
+        inputRead();
+        switch (currentState)
+        {
+            case playerState.idle:
+                break;
+            case playerState.death:
+                break;
+            case playerState.walkUp:
+                break;
+            case playerState.walkDown:
+                break;
+            case playerState.walkLeft:
+                break;
+            case playerState.walkRight:
+                break;
+            case playerState.punch:
+                break;
+        }
+    }
+
+    private void inputRead()
+    {
+        if (!Input.anyKey) { currentState = playerState.idle; }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.W)) { currentState = playerState.walkUp; }
+            if (Input.GetKeyDown(KeyCode.S)) { currentState = playerState.walkDown; }
+            if (Input.GetKeyDown(KeyCode.A)) { currentState = playerState.walkLeft; }
+            if (Input.GetKeyDown(KeyCode.D)) { currentState = playerState.walkRight; }
+        }
+    }
+
+}
