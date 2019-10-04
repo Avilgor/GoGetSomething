4f6c62b76d3336b54da0f3d2d3ecd82467f14fb1
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-10-05 01:58:47 +0200

    Movement

--- GoGetSomething/Assets/Scripts/Movement.cs
+++ GoGetSomething/Assets/Scripts/Movement.cs
@@ -16,45 +16,67 @@ public class Movement : MonoBehaviour
 
     }
 
-    playerState currentState;
+    Rigidbody2D rb;
+    playerState currentState,newState;
+    private float velocity;
 
     void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody2D>();
         currentState = playerState.idle;
+        velocity = 4;
     }
 
     
     void Update()
     {
         inputRead();
-        switch (currentState)
+        if (currentState != newState)
         {
-            case playerState.idle:
-                break;
-            case playerState.death:
-                break;
-            case playerState.walkUp:
-                break;
-            case playerState.walkDown:
-                break;
-            case playerState.walkLeft:
-                break;
-            case playerState.walkRight:
-                break;
-            case playerState.punch:
-                break;
+            switch (newState)
+            {
+                case playerState.idle:
+                    rb.velocity = new Vector2(0, 0);
+                    break;
+                case playerState.death:
+                    gameObject.SetActive(false);
+                    break;
+                case playerState.walkUp:
+                    rb.velocity = new Vector2(0, 0);
+                    if (rb.velocity.y < 4)
+                        rb.velocity += new Vector2(0, velocity);
+                    break;
+                case playerState.walkDown:
+                    rb.velocity = new Vector2(0, 0);
+                    if (rb.velocity.y > -4)
+                        rb.velocity += new Vector2(0, -velocity);
+                    break;
+                case playerState.walkLeft:
+                    rb.velocity = new Vector2(0, 0);
+                    if (rb.velocity.x > -4)
+                        rb.velocity += new Vector2(-velocity, 0);
+                    break;
+                case playerState.walkRight:
+                    rb.velocity = new Vector2(0, 0);
+                    if (rb.velocity.x < 4)
+                        rb.velocity += new Vector2(velocity, 0);
+                    break;
+                case playerState.punch:
+                    break;
+            }
+            currentState = newState;
         }
     }
 
     private void inputRead()
     {
-        if (!Input.anyKey) { currentState = playerState.idle; }
+        if (!Input.anyKey) { newState = playerState.idle; }
         else
         {
-            if (Input.GetKeyDown(KeyCode.W)) { currentState = playerState.walkUp; }
-            if (Input.GetKeyDown(KeyCode.S)) { currentState = playerState.walkDown; }
-            if (Input.GetKeyDown(KeyCode.A)) { currentState = playerState.walkLeft; }
-            if (Input.GetKeyDown(KeyCode.D)) { currentState = playerState.walkRight; }
+            if (Input.GetKeyDown(KeyCode.W)) { newState = playerState.walkUp; }
+            if (Input.GetKeyDown(KeyCode.S)) { newState = playerState.walkDown; }
+            if (Input.GetKeyDown(KeyCode.A)) { newState = playerState.walkLeft; }
+            if (Input.GetKeyDown(KeyCode.D)) { newState = playerState.walkRight; }
         }
     }
 
