dec6cd0e812bceb9642d52d9186e45cc21d3ea7d
Axel <acr@iboomobile.com>
2019-10-05 20:37:22 +0200

    NavMesh 2D Pathfinder

---
+++ GoGetSomething/Assets/Enemy.cs
@@ -0,0 +1,21 @@
+/**
+ * Enemy.cs
+ * Created by Akeru on 05/10/2019
+ */
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    #region Fields
+
+    #endregion
+
+    #region MonoBehaviour Functions
+
+    #endregion
+
+    #region Other Functions
+
+    #endregion
+}
---
+++ GoGetSomething/Assets/Scripts/Chase.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chase : MonoBehaviour
+{
+    public GameObject target;
+    private NavMeshAgent agent;
+    private bool stop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            agent.speed += 0.1f;
+        }
+        if ((transform.position - target.transform.position).magnitude < 1 && !stop)
+        {
+            stop = true;
+            target.GetComponent<Navigate>().enabled = false;
+        }
+        agent.SetDestination(target.transform.position);
+        Navigate.DebugDrawPath(agent.path.corners);
+    }
+}
