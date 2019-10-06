17610367f784d265b0c9dd03e77e1977be75f3cb
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-10-06 18:04:55 +0200

    Enemy animations

--- GoGetSomething/Assets/Scripts/Enemy.cs
+++ GoGetSomething/Assets/Scripts/Enemy.cs
@@ -6,6 +6,14 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+enum lookPoint
+{
+    upLeft=0,
+    upRight,
+    downRight,
+    downLeft
+}
+
 public enum EnemyType
 {
     Null = -1,
@@ -18,9 +26,11 @@ public class Enemy : MonoBehaviour
 {
     #region Fields
     [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Animator _anim;
 
     private bool _stop;
     private GameObject _target;
+    private lookPoint directionPointer;
 
     #endregion
 
@@ -28,11 +38,28 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         InitSetup();
+        directionPointer = lookPoint.downRight;
     }
 
     private void Update()
     {
         FindUpdate();
+        directionPointer = CalculateAngleDirection();
+        switch (directionPointer)
+        {
+            case lookPoint.upLeft:
+                _anim.SetFloat("Blend",1f);
+                break;
+            case lookPoint.upRight:
+                _anim.SetFloat("Blend",0.66f);
+                break;
+            case lookPoint.downRight:
+                _anim.SetFloat("Blend",0.33f);
+                break;
+            case lookPoint.downLeft:
+                _anim.SetFloat("Blend",0f);
+                break;
+        }
     }
 
     #endregion
@@ -55,7 +82,18 @@ public class Enemy : MonoBehaviour
             //TODO Hit
         }
         _agent.SetDestination(_target.transform.position);
-        Navigate.DebugDrawPath(_agent.path.corners);
+        Navigate.DebugDrawPath(_agent.path.corners);        
+    }
+
+    private lookPoint CalculateAngleDirection()
+    {
+        Vector3 v2 = transform.position - _target.transform.position;
+        float angle = Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg;
+        if (angle >= -180 && angle <-90) return lookPoint.upRight;
+        else if (angle < 0 && angle > -91) return lookPoint.upLeft;
+        else if (angle > 0 && angle < 90) return lookPoint.downLeft;
+        else if (angle > 90 && angle <= 180) return lookPoint.downRight;
+        else return lookPoint.downRight;
     }
 
     #endregion
