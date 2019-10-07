21299d00d0a689b9d5207b896fb0c3a5724e8f7d
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-10-07 14:13:58 +0200

    intro sound

--- GoGetSomething/Assets/Scripts/IntroScript.cs
+++ GoGetSomething/Assets/Scripts/IntroScript.cs
@@ -7,17 +7,19 @@ using UnityEngine.SceneManagement;
 public class IntroScript : MonoBehaviour
 {
     VideoPlayer player;
+    [SerializeField]AudioClip sound;
     private bool started,finished;
     void Start()
     {
         player = GetComponent<VideoPlayer>();
         started = false;
         finished = false;
+        GetComponent<AudioSource>().PlayOneShot(sound);
     }
 
     void Update()
-    {      
-        if (player.isPlaying)
+    {
+        if (player.isPlaying)       
             started = true;
 
         if (!player.isPlaying && started)
